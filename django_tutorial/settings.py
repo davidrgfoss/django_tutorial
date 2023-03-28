14e4ebbd6f856ec5d0211e3114225f1544c2e015
davidrg <davidrg@python.davidrg.gonzalonazareno.org>
2023-03-28 13:32:16 +0200

    Examen

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db',
-        'USER': 'django',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
